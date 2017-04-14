27cccdfe20dd160a68a6694326748710ec22d4cf
LukeMurphey <Luke@LukeMurphey.net>
2017-04-14 15:58:55 -0500

    Creating the custom setup view

---
+++ src/appserver/static/js/views/CustomSetupView.js
@@ -0,0 +1,44 @@
+require.config({
+    paths: {
+        setup_view: '../app/simplexml_setup/js/views/SetupView'
+    }
+});
+
+define([
+    "underscore",
+    "backbone",
+    "jquery",
+    "setup_view",
+], function(
+    _,
+    Backbone,
+    $,
+    SetupView
+){
+
+    return SetupView.extend({
+        className: "CustomSetupView",
+
+        events: {
+            "click #save-config" : "saveConfig"
+        },
+
+        initialize: function() {
+        	this.options = _.extend({}, this.defaults, this.options);
+            SetupView.prototype.initialize.apply(this, [this.options]);
+        },
+
+        saveConfig: function(){
+            if(this.userHasAdminAllObjects()){
+                this.setConfigured();
+            }
+            else{
+                alert("You don't have permission to edit this app");
+            }
+        },
+        
+        render: function () {
+            this.$el.html('<a href="#" class="btn btn-primary" id="save-config">Save Configuration</a>');
+        }
+    });
+});
